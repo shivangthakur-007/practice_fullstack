666458b2a658f20cd9d287e7b31a4b4d284eb373
Dance <thakurshivang579@gmail.com>
2023-09-29 11:31:31 +0530

    ninth commit

--- practice_client/src/App.jsx
+++ practice_client/src/App.jsx
@@ -1,36 +1,8 @@
 import Layouts from './Layouts/Layouts';
 import A from './Components/A';
 import {Route, Routes} from 'react-router-dom';
-import { createContext } from 'react';
 
-// const Bio=createContext();
 function App() {
-  // const Carousel_Pics = [
-  //   {
-  //     Carousel_IMG_1:
-  //       "https://www.combonation.in/storage/55409/6503f33f973ed_hero-bannerArtboard-11.jpg",
-  //   },
-  //   {
-  //     Carousel_IMG_2:
-  //       "https://www.combonation.in/storage/55411/6503f3cf2ef5c_hero-bannerArtboard-12.jpg",
-  //   },
-  //   {
-  //     Carousel_IMG_3:
-  //       "https://www.combonation.in/storage/55413/6503f405c92ab_hero-bannerArtboard-9.jpg",
-  //   },
-  //   {
-  //     Carousel_IMG_4:
-  //       "https://www.combonation.in/storage/51150/64c9f33b7217b_hero-banner-Recovered-RecoveredArtboard-10-copy-4.jpg",
-  //   },
-  //   {
-  //     Carousel_IMG_5:
-  //       "https://www.combonation.in/storage/55877/650bdb66d04a6_combonation-banner-m-01(1)_11zon-(1).jpg",
-  //   },
-  //   {
-  //     Carousel_IMG_6:
-  //       "https://www.combonation.in/storage/55407/6503f30af238d_hero-bannerArtboard-10.jpg",
-  //   },
-  // ];
   return (
     <>
         <Routes>
--- practice_client/src/Layouts/Layouts.jsx
+++ practice_client/src/Layouts/Layouts.jsx
@@ -13,11 +13,14 @@ import Carousel from "../Components/Carousel";
 function Layouts() {
   const img =
     "https://www.combonation.in/storage/9453/63ca5f25af222_Xmas_logo_theme.png";
-    const img1="https://www.combonation.in/storage/55399/6501ac9b11108_BYOC_Strip_banner_Sep_DT.jpg"
+  const img1 =
+    "https://www.combonation.in/storage/55399/6501ac9b11108_BYOC_Strip_banner_Sep_DT.jpg";
+  const img2 =
+    "https://www.combonation.in/storage/56834/6512dbb29b553_MKQ_banner_DT_oct.jpg";
   return (
     <div className="min-h-[100vh]  font-sans">
       <section className="bg-[#fd7e14] leading-4 p-[2px] max-h-[8vh] flex items-center justify-around">
-        <div className="w-[50%] pl-[2rem]">
+        <div className="w-[50%] pl-[2rem] py-2">
           <a href="/" className="text-lg font-semibold text-white">
             why choose Combination?
           </a>
@@ -39,13 +42,13 @@ function Layouts() {
           </ul>
         </div>
       </section>
-      <header className="flex flex-wrap max-w-[100%] mx-[7rem]">
+      <header className="flex flex-wrap max-w-[100%] mx-[7rem] relative">
         <div className="flex  items-center grow-0 basis-[25%] justify-start">
           <a href="/">
             <img src={img} width="150px" />
           </a>
         </div>
-        <div className="flex grow-0 basis-[50%] items-center text-[16px] justify-start px-[2rem]  font-[600] text-slate-700">
+        <div className="flex grow-0 basis-[50%] items-center text-[14px] justify-start px-[2rem]  font-[700] text-slate-700">
           <div className="flex gap-[2rem] ">
             <a href="/">Home</a>
             <a href="/">
@@ -64,37 +67,39 @@ function Layouts() {
           <span>+918595091624</span>
         </div>
       </header>
-      <nav className="flex mx-[7rem] my-[1rem] flex-wrap ">
-        <div className="flex grow-0 basis-[25%] py-[2px]"></div>
-        <div className="flex grow-0 basis-[50%] pl-[1rem]">
-          <div className="box-border w-[100%] border-none rounded-md  mr-[2rem] ">
-            <input
-              className="bg-transparent w-[100%]   py-[0.3rem] px-[1rem] text-lg font-semibold"
-              type="search"
-              name="search_box"
-              id="search_box"
-              placeholder="Search Over 10700+ Products"
-            />
+      <div className="sticky top-0 left-0 bg-white w-full py-[3px]">
+        <nav className="flex flex-wrap pb-[0.5rem] ml-[7rem] mr-[5rem]   ">
+          <div className="flex grow-0 basis-[25%] "></div>
+          <div className="flex grow-0 basis-[50%] pl-[1rem]">
+            <div className="box-border w-[100%] border-none rounded-md  mr-[2rem] ">
+              <input
+                className="bg-transparent rounded-md  w-[100%]   py-[0.3rem] px-[1rem] text-lg font-semibold"
+                type="search"
+                name="search_box"
+                id="search_box"
+                placeholder="Search Over 10700+ Products"
+              />
+            </div>
           </div>
-        </div>
-        <ul className="flex grow-0 basis-[20%]">
-          <a href="/">
-            <li>
-              <CgProfile />
-            </li>
-          </a>
-          <a href="/">
-            <li>
-              <AiOutlineHeart />
-            </li>
-          </a>
-          <a href="/">
-            <li>
-              <BsBag />
-            </li>
-          </a>
-        </ul>
-      </nav>
+          <ul className="flex grow-0 basis-[20%] items-center gap-[1rem] justify-end ">
+            <a href="/">
+              <li>
+                <CgProfile className="w-[40px] h-[35px]" />
+              </li>
+            </a>
+            <a href="/">
+              <li>
+                <AiOutlineHeart className="w-[40px] h-[35px]" />
+              </li>
+            </a>
+            <a href="/">
+              <li>
+                <BsBag className="w-[40px] h-[30px]" />
+              </li>
+            </a>
+          </ul>
+        </nav>
+      </div>
       <div className="carousel flex gap-[3rem] bg-[#FFF4E3] text-xl text-slate-500 p-[8px] font-semibold">
         <div className="carousel-item">
           <a href="/">
@@ -153,13 +158,18 @@ function Layouts() {
         </div>
       </div>
       <section>
-        <div><img src={img1} /></div>
+        <div>
+          <img src={img1} />
+        </div>
       </section>
       <nav>
         <Carousel />
       </nav>
+      <div>
+        <img src={img2} alt="new" />
+      </div>
     </div>
   );
 }
 
-export default Layouts
+export default Layouts;
