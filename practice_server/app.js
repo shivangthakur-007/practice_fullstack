217d14312baad999f203615ef1005e64e502d87d
Dance <thakurshivang579@gmail.com>
2023-10-10 18:48:35 +0530

    fifth commit

--- practice_server/app.js
+++ practice_server/app.js
@@ -1,8 +1,20 @@
 import  express  from "express";
-
+import morgan from "morgan";
+import cors from "cors"
 var app=express();
 
+app.use(express.json());
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
+
 app.use('/', (req, res)=>{
-    res.send('/pong')
+    res.send('/pong');
 })
+app.use(morgan('dev'));
+
 export default app;
